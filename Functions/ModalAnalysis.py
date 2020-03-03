4db64f1ddb22c84a46349cf514cb7b2d478ae638
Yoimi KOJIMA <y-kojima@st.go.tuat.ac.jp>
2020-03-02 16:11:32 -0800

    Add comments for BoundaryCondition.py

--- Functions/ModalAnalysis.py
+++ Functions/ModalAnalysis.py
@@ -15,6 +15,17 @@ from Functions.Gradient import Gradient
 
 class ModalData(FieldData):
     def __init__(self, mesh, operator_name='matL.npz', n_val=5, k=10, mpi_comm=None, **kwargs):
+        """
+        Abstract class for modal analysis.
+
+        :param mesh: Mesh class.
+        :param operator_name: File name of the liner operator.
+        :param n_val: Number of variables. Default: 5.
+        :param k: Number of modes for each calculations.
+        :param mpi_comm: MPI communicator.
+        :param kwargs: Options.
+        """
+
         # kwargs for set up the operator.
         self._k = k
         self._n_q = n_val
@@ -46,15 +57,34 @@ class ModalData(FieldData):
         self.data = np.empty((self.n_cell, self._data_num()), dtype=np.float64)
 
     def _data_num(self):
+        """
+        Calculate number of data stored self.data.
+
+        :return: Number of data.
+        """
+
         raise NotImplementedError
 
     def _data_name_list(self):
+        """
+        Generate list of data name for visualization.
+
+        :return: Name list.
+        """
+
         raise NotImplementedError
 
     def _set_operator(self, operator, **kwargs):
         raise NotImplementedError
 
     def solve(self, **kwargs):
+        """
+        Solve problems.
+
+        :param kwargs:
+        :return:
+        """
+
         self._vec_data = self._calculate(**kwargs)
         self._set_data(self._vec_data)  # Set self.data and self._vec_data for the visualization.
 
@@ -70,11 +100,29 @@ class ModalData(FieldData):
         raise NotImplementedError
 
     def _set_data(self, data):
+        """
+        Set data for self.data.
+
+        :param data: Moe data.
+        """
+
         raise NotImplementedError
 
 
 class LinearStabilityMode(ModalData):
     def __init__(self, mesh, operator, n_val=5, k=5, n_grid=10, add_pres=False, **kwargs):
+        """
+        Class for global stability analysis.
+        This class does not support MPI computations.
+
+        :param mesh: Mesh class.
+        :param operator_name: File name of the liner operator.
+        :param n_val: Number of variables. Default: 5.
+        :param k: Number of modes for each calculations.
+        :param n_grid: Number of grid points used for shift-inverted mode.
+        :param add_pres: Flag for visualized data of pressure mode.
+        """
+
         self._n_grid = n_grid
         self._add_pres = add_pres
 
@@ -83,6 +131,9 @@ class LinearStabilityMode(ModalData):
 
         super(LinearStabilityMode, self).__init__(mesh, operator, n_val, k, **kwargs)
         self._arpack_options.update(**kwargs)
+
+        if self._mpi:
+            raise Exception('This class does not support MPI computations.')
 
     def _data_num(self):
         return self._n_q * self._k * self._n_grid
@@ -104,8 +155,8 @@ class LinearStabilityMode(ModalData):
 
         eig_list = []
         vec_list = []
-        for i_grid, (s_real, s_imag) in enumerate(grid_list):
-            sigma = s_real + 1.0j * s_imag
+        for i_grid, (sigma_real, sigma_imag) in enumerate(grid_list):
+            sigma = sigma_real + 1.0j * sigma_imag
             self._arpack_options.update({'sigma': sigma})
             print('Sigma =', sigma)
 
@@ -155,12 +206,26 @@ class LinearStabilityMode(ModalData):
 
 class ResolventMode(ModalData):
     def __init__(self, mesh, ave_field, operator, n_val=5, k=6, mode=None, mpi_comm=None, **kwargs):
+        """
+        Class for resolvent analysis.
+
+        :param mesh: Mesh class.
+        :param ave_field: Time averaged flow field. Used to calculate Tu norm.
+        :param operator: File name of the operator.
+        :param n_val: Number of variables.
+        :param k: Number of mode for each calculations.
+        :param mode: Mode to be calculated. 'Forcing' or 'Response' or 'Both'.
+        :param mpi_comm: MPI communicator.
+        :param kwargs: Options.
+        """
+
         self._ave_field = ave_field
 
         self._mode = mode  # 'F' for the forcing mode or 'R' for the response mode. 'None' will get both.
         self._mode_f = mode == 'Both' or mode == 'Forcing'
         self._mode_r = mode == 'Both' or mode == 'Response'
 
+        # Matrix for the numerical quadrature.
         self._qi = None
         self._qo = None
         self._resolvent = None
@@ -179,7 +244,7 @@ class ResolventMode(ModalData):
             print('Start resolvent operations.')
 
         t_start = time.time()
-        for i_grid, grid in self._grid_queue(grid_list):
+        for i_grid, grid in self._grid_queue(grid_list):  # Calculate resolvent modes for each omega and alpha values.
             if grid is not None:
                 omega, alpha = grid
 
@@ -198,11 +263,12 @@ class ResolventMode(ModalData):
                 w_list = None
 
             if self._mpi:
-                gains_list = self._comm.gather(w_list, root=0)
+                gains_list = self._comm.gather(w_list, root=0)  # Gather list of gain from threads to root.
             else:
                 gains_list = [w_list]
 
             if self._is_root:
+                # Write gains list to text file.
                 if not all(item is None for item in gains_list):
                     data_list = [item for item in gains_list if item is not None]
                     data_list.sort()
@@ -230,12 +296,20 @@ class ResolventMode(ModalData):
             if self._mpi:
                 stop_operation = self._comm.bcast(stop_operation, root=0)
 
+            # Continue operation or not.
             if stop_operation:
                 break
             else:
                 continue
 
     def _grid_queue(self, grid_list):
+        """
+        Generate grid queue for parallel computations.
+
+        :param grid_list: List of grid contains omega and alpha.
+        :return: Grid data.
+        """
+
         i_step = self._size
         i_ind = self._rank % self._size
 
@@ -292,6 +366,12 @@ class ResolventMode(ModalData):
         return 1.0 / np.sqrt(svs), self._qi @ mode_r, self._qi @ mode_f
 
     def _set_data(self, data):
+        """
+        Set data arrays for resolvent analysis.
+
+        :param data: Mode vectors.
+        """
+
         _, _, _, r_vecs, f_vecs = data
 
         coef_ind_1 = 1 + int(self._mode == 'Both')
@@ -311,7 +391,13 @@ class ResolventMode(ModalData):
                 i_end = i_start + self._n_q
                 self.data[:, i_start:i_end] = np.real(rw_vec)
 
-    def _get_norm_quadrature(self):  # Chu's energy norm.
+    def _get_norm_quadrature(self):
+        """
+        Calculate numerical quadrature by Chu's energy norm.
+
+        :return:
+        """
+
         ave_data = self._ave_field.data
         rho_data = ave_data[:, 0]
         t_data = ave_data[:, 4]
@@ -319,7 +405,8 @@ class ResolventMode(ModalData):
         gamma = 1.4  # heat ratio
         r_gas = 1.0 / 1.4  # Non-dimensionalized gas constant.
 
-        vols = self.mesh.volumes / np.linalg.norm(self.mesh.volumes)
+        vols = self.mesh.volumes / np.linalg.norm(self.mesh.volumes)  # Weights by cell volumes.
+
         diag_rho = vols * r_gas * t_data / rho_data
         diag_u = vols * rho_data
         diag_t = vols * r_gas * rho_data / ((gamma - 1) * t_data)
@@ -333,6 +420,20 @@ class ResolventMode(ModalData):
 
 class RandomizedResolventMode(ResolventMode):
     def __init__(self, mesh, bd_cond, ave_field, operator, n_val=5, k=6, mode='Both', **kwargs):
+        """
+        Class for calculating Randomized resolvent analysis.
+        Jean Hélder Marques Ribeiro, Chi-An Yeh, Kunihiko Taira, 2020, https://arxiv.org/abs/1902.01458.
+
+        :param mesh: Mesh class.
+        :param ave_field: Time averaged flow field. Used to calculate Tu norm.
+        :param operator: File name of the operator.
+        :param n_val: Number of variables.
+        :param k: Number of mode for each calculations.
+        :param mode: Mode to be calculated. 'Forcing' or 'Response' or 'Both'.
+        :param mpi_comm: MPI communicator.
+        :param kwargs: Options.
+        """
+
         super(RandomizedResolventMode, self).__init__(mesh, ave_field, operator, n_val, k, mode, **kwargs)
 
         self._scaling = self._get_scaling_factor(ave_field.data, bd_cond)
@@ -369,32 +470,18 @@ class RandomizedResolventMode(ResolventMode):
         return Sigma, self._qi @ U, self._qi @ V
 
 
-class ResolventTest(RandomizedResolventMode):
-    def __init__(self, mesh, ave_field, operator, n_val=5, k=6, mode='Both', **kwargs):
-        super(RandomizedResolventMode, self).__init__(mesh, ave_field, operator, n_val, k, mode, **kwargs)
-
-    def _calculate(self, resolvent):
-        resolvent_lu = linalg.splu(resolvent)
-
-        m = self.n_cell * 5  # = resolvent.shape[0]
-        k = self._k  # Number of mode
-        matO = self._scaling @ np.random.normal(0.0, 0.1, (m, k))
-        matY = resolvent_lu.solve(matO)
-
-        matQ, _ = sp.linalg.qr(matY, mode='economic')
-        matB = resolvent_lu.solve(matQ, trans='H')
-
-        _, _, V = sp.linalg.svd(matB.T.conj(), full_matrices=False)
-        matUS = resolvent_lu.solve(V.T.conj())
-
-        U, Sigma, Vapp = sp.linalg.svd(matUS.conj(), full_matrices=False)
-        V = V.T.conj() @ Vapp.T.conj()
-
-        return Sigma, U, V
-
-
 class RHS(FieldData):
     def __init__(self, mesh, field, mu, pr, is2d=False):
+        """
+        Calculate right hand side of LNS operator.
+
+        :param mesh: Mesh class.
+        :param field: Field data.
+        :param mu: viscosity.
+        :param pr: Prandtl number.
+        :param is2d: Flag for 2D computation.
+        """
+
         super(RHS, self).__init__(mesh, n_val=5, data_list=['Rho', 'U', 'V', 'W', 'T'])
 
         self.rhs_ns = LNS(mesh, field, mu, pr, is2d)
